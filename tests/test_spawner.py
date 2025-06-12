09b87ba8c101c83331fb1def1736b37985c8b81f
Enol Fernandez <enol.fernandez@egi.eu>
2025-06-12 15:52:26 +0100

    Add more test cases

--- tests/test_spawner.py
+++ tests/test_spawner.py
@@ -6,7 +6,6 @@ from d4science_hub.spawner import D4ScienceSpawner
 
 @pytest.mark.asyncio
 async def test_build_options_role():
-    dummy_user = DummyUser()
     spawner = D4ScienceSpawner(_mock=True)
     roles = ["foo-role"]
     resources = {
@@ -31,10 +30,20 @@ async def test_build_options_role():
             ]
         }
     }
+    # role of user and resource matches
     server_opts, vol_opts = spawner.build_resource_options(roles, resources)
     assert "witoil-authid" in server_opts.keys()
     assert vol_opts == {}
 
+    # role of user and resource does not match
     server_opts, vol_opts = spawner.build_resource_options([], resources)
     assert "witoil-authid" not in server_opts.keys()
+    assert vol_opts == {}
+
+    # no role in the resource
+    del resources["genericResources"]["Resource"][0]["Profile"]["Body"]["ServerOption"][
+        "@role"
+    ]
+    server_opts, vol_opts = spawner.build_resource_options([], resources)
+    assert "witoil-authid" in server_opts.keys()
     assert vol_opts == {}
