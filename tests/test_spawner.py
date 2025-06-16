57487432116cfe432db3eaa074e2dc7924d77055
Enol Fernandez <enol.fernandez@egi.eu>
2025-06-16 14:27:43 +0100

    Add tests for url generation

--- tests/test_spawner.py
+++ tests/test_spawner.py
@@ -1,10 +1,9 @@
 """Tests for the spawner"""
 
-from unittest.mock import MagicMock
+from unittest import mock
 
 import pytest
 from d4science_hub.spawner import D4ScienceSpawner
-
 
 
 @pytest.mark.asyncio
@@ -55,7 +54,31 @@ async def test_build_options_role():
 @pytest.mark.asyncio
 async def test_custom_user_options():
     spawner = D4ScienceSpawner(_mock=True)
-    user_options = MagicMock()
+    user_options = mock.MagicMock()
     spawner.custom_user_options = user_options
     await spawner.load_user_options()
     user_options.assert_called_once_with(spawner)
+
+
+@pytest.mark.asyncio
+async def test_get_args():
+    spawner = D4ScienceSpawner(_mock=True)
+    spawner.default_url = "/foo"
+
+    # if not RStudio
+    args = spawner.get_args()
+    assert set(
+        ["--SingleUserNotebookApp.default_url=/foo", "--ServerApp.default_url=/foo"]
+    ).issubset(args)
+
+    # if RStudio
+    with mock.patch("d4science_hub.spawner.D4ScienceSpawner.orm_spawner") as m_orm:
+        m_orm.name = "RStudio"
+        spawner.default_url = "/foo"
+        args = spawner.get_args()
+        assert set(
+            [
+                "--SingleUserNotebookApp.default_url=/rstudio",
+                "--ServerApp.default_url=/rstudio",
+            ]
+        ).issubset(args)
